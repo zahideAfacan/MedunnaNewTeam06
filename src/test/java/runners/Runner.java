ed26a0c42c2beff92414e2b00974e89327508a28
Kemal Ucler <kemalucler@gmail.com>
2023-03-13 00:18:10 -0400

    m1

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
 
 
         features = "src/test/resources/features",
-        glue={"stepdefinitions","hooks"},
+        glue={"stepdefinitions", "utilities/hooks"},
         dryRun =false,
         tags = "@US021"
 
--- src/test/java/utilities/hooks/Hooks.java
+++ src/test/java/utilities/hooks/Hooks.java
@@ -1,4 +1,4 @@
-package hooks;
+package utilities.hooks;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
